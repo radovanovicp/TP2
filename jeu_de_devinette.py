c194cbec5bde3ee2e98d46609ad7c3757629e320
radovanovicp <112640256+radovanovicp@users.noreply.github.com>
2022-10-13 08:41:38 -0400

    Update jeu_de_devinette.py

--- jeu_de_devinette.py
+++ jeu_de_devinette.py
@@ -1,26 +1,31 @@
 import random
 
-print("J'ai choisi un nombre entre 0 et 1000. À vous de le devinez!")
+def jeu():
 
-nombre = random.randint(0, 1000)
-print(nombre)
-essai = int(input("Entrez votre essai: "))
-nbr_essai = 0
+  borne_minimale = int(input("Bienvenue au jeu de devinette! Choisissez une borne minimale."))
+  borne_maximale = int(input("Bienvenue au jeu de devinette! Choisissez une borne maximale."))
+
+  nombre = random.randint(borne_minimale, borne_maximale)
+  essai = int(input("Entrez votre essai: "))
+  nbr_essai = 0
 
 
-while essai != nombre:
-  nbr_essai += 1
-  if essai < nombre:
-   essai = int(input("Votre essai est inférieure au nombre"))
+  while essai != nombre:
+    nbr_essai += 1
+    if essai < nombre:
+     essai = int(input("Votre essai est inférieure au nombre"))
 
-  elif essai > nombre:
-    essai = int(input("Votre essai est supérieure au nombre"))
+    elif essai > nombre:
+      essai = int(input("Votre essai est supérieure au nombre"))
 
-    if essai == nombre:
-      print("Félicitations, vous avez devinez le nombre en" + nbr_essai + "essaies!")
-      rejouer = input("Voulez-vous jouez une autre partie? (oui ou non)")
-        if rejouer: "oui"
-
+  while essai == nombre:
+    print("Félicitations, vous avez devinez le nombre en " + str(nbr_essai) + " essaies!")
+    rejouer = input("Voulez-vous jouez une autre partie? (oui ou non)")
+    if rejouer == "non":
+        exit()
+    if rejouer == "oui":
+        jeu()
+jeu()
 
 
 
